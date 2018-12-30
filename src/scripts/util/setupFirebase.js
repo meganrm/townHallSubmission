43d81fb2fc699a50e9840937ef3a5ab2cf43691e
meganrm <meganrm@gmail.com>
2018-12-30 14:19:29 -0800

    fixing errors

--- src/scripts/util/setupFirebase.js
+++ src/scripts/util/setupFirebase.js
@@ -7,8 +7,8 @@ const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.DATABASE_URL,
-    storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
 };
 
 firebase.initializeApp(config);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 require('dotenv').config();
-const path = require('path');
-const fs = require('fs');
 
 const {
     EnvironmentPlugin,
