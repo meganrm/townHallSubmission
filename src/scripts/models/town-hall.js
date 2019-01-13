a0c5e21c1b9c33848856b96a5cac2bb8206cd6ca
meganrm <meganrm@gmail.com>
2019-01-13 13:10:25 -0800

    small fixes

--- src/scripts/models/town-hall.js
+++ src/scripts/models/town-hall.js
@@ -53,31 +53,30 @@ class TownHall {
             .then((r) => {
                 const response = r.body;
                 if (!response.timeZoneName) {
-                    Error('no timezone results', id, response);
-                } else {
-                    newTownHall.zoneString = response.timeZoneId;
-                    const timezoneAb = response.timeZoneName.split(' ');
-                    newTownHall.timeZone = timezoneAb.reduce((acc, cur) => {
-                        acc += cur[0];
-                        return acc;
-                    }, '');
-                    const offset = response.rawOffset / 60 / 60 + response.dstOffset / 60 / 60;
-                    let utcoffset;
-                    if (parseInt(offset) === offset) {
-                        utcoffset = `UTC${offset}00`;
-                    } else {
-                        const fract = offset * 10 % 10 / 10;
-                        const integr = Math.trunc(offset);
-                        let mins = (Math.abs(fract * 60)).toString();
-                        const zeros = '00';
-                        mins = zeros.slice(mins.length) + mins;
-                        utcoffset = `UTC${integr}${mins}`;
-                    }
-
-                    console.log(offset, `${newTownHall.Date.replace(/-/g, '/')} ${databaseTH.Time} ${utcoffset}`);
-                    newTownHall.dateObj = new Date(`${newTownHall.Date.replace(/-/g, '/')} ${databaseTH.Time} ${utcoffset}`).getTime();
-                    return newTownHall;
+                    return Error('no timezone results', id, response);
                 }
+                newTownHall.zoneString = response.timeZoneId;
+                const timezoneAb = response.timeZoneName.split(' ');
+                newTownHall.timeZone = timezoneAb.reduce((acc, cur) => {
+                    acc += cur[0];
+                    return acc;
+                }, '');
+                const offset = response.rawOffset / 60 / 60 + response.dstOffset / 60 / 60;
+                let utcoffset;
+                if (parseInt(offset) === offset) {
+                    utcoffset = `UTC${offset}00`;
+                } else {
+                    const fract = offset * 10 % 10 / 10;
+                    const integr = Math.trunc(offset);
+                    let mins = (Math.abs(fract * 60)).toString();
+                    const zeros = '00';
+                    mins = zeros.slice(mins.length) + mins;
+                    utcoffset = `UTC${integr}${mins}`;
+                }
+
+                console.log(offset, `${newTownHall.Date.replace(/-/g, '/')} ${databaseTH.Time} ${utcoffset}`);
+                newTownHall.dateObj = new Date(`${newTownHall.Date.replace(/-/g, '/')} ${databaseTH.Time} ${utcoffset}`).getTime();
+                return newTownHall;
             }).catch((error) => {
                 console.log(error);
             });
@@ -103,16 +102,19 @@ class TownHall {
                 address,
             })
             .then((r) => {
-                console.log(r.body.results[0]);
+                console.log(r.body, r.body.results[0]);
                 const { results } = r.body;
                 if (results) {
                     newTownHall.lat = results[0].geometry.location.lat;
                     newTownHall.lng = results[0].geometry.location.lng;
-                    newTownHall.address = results[0].formatted_address.split(', USA')[0];  
+                    newTownHall.address = results[0].formatted_address.split(', USA')[0];
                     console.log(newTownHall);
                     return newTownHall;
                 }
                 return Promise.reject('error geocoding', newTownHall);
+            })
+            .catch((error) => {
+                console.log('error geocoding', error);
             });
     }
 }
