ee6f91ebd778353bf9bd0ddcfb483a7c681ca5ee
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-04 16:05:44 -0700

    update App main layout (header comp. styles, etc.)

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import classNames from 'classnames';
 import {
   Layout,
+  Affix,
+  Icon,
 } from 'antd';
 
+import PageHeader from '../../components/PageHeader';
 import MainFormController from '../FormController';
 import SideBar from '../SideBar';
 import './style.scss';
 
 const {
-  Header,
   Sider,
   Content,
 } = Layout;
@@ -17,47 +19,87 @@ const {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.onCollapse = this.onCollapse.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.handleWindowSizeChange = this.handleWindowSizeChange.bind(this);
     this.state = {
       collapsed: false,
+      width: window.innerWidth,
     };
   }
 
-  onCollapse(collapsed) {
-    console.log(collapsed);
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  toggle() {
     this.setState({
-      collapsed,
+      collapsed: !this.state.collapsed,
     });
   }
 
   render() {
-    const { collapsed } = this.state;
+    const {
+      collapsed,
+      width
+    } = this.state;
+    const isMobile = width <= 500;
     const contentClass = classNames('body-content', {
-      'side-bar-open': !collapsed,
+      'side-bar-closed': collapsed,
     });
-    return (
-      <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsible
-          width={250}
-          collapsedWidth="0"
-          collapsed={collapsed}
-          onCollapse={this.onCollapse}
-          style={{
-            height: '100vh',
-            left: 0,
-          }}
-        >
-          <SideBar />
-        </Sider>
-        <Layout className={contentClass}>
-          <Content>
-            <MainFormController />
-          </Content>
+    if (isMobile) {
+      return (
+        <div>
+          <PageHeader />
+          Mobile!!
+        </div>
+      );
+    } else {
+      return (
+        <Layout>
+          <PageHeader />
+          <Layout>
+            <Layout>
+              <Affix>
+                <Sider
+                  breakpoint="lg"
+                  collapsible
+                  width={250}
+                  collapsedWidth="0"
+                  collapsed={collapsed}
+                  style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    marginBottom: 40,
+                  }}
+                >
+                  <SideBar />
+                </Sider>
+              </Affix>
+              <Affix>
+                <Icon
+                  className={`${contentClass} trigger`}
+                  type={collapsed ? 'menu-unfold' : 'menu-fold'}
+                  onClick={this.toggle}
+                />
+              </Affix>
+              <Content className={contentClass}>
+                <MainFormController />
+              </Content>
+            </Layout>
+          </Layout>
         </Layout>
-      </Layout>
-    );
+      );
+    }
   }
 }
 
