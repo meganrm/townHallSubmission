530f23fa1e3b37d8a67b665c3f6410867f247246
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-13 09:31:27 -0700

    add usermoc selector

--- src/state/user/selectors.js
+++ src/state/user/selectors.js
@@ -1,3 +1,4 @@
 export const getUid = state => state.user.uid;
 export const getUserEmail = state => state.user.email;
 export const getUserName = state => state.user.displayName;
+export const getUserMOCs = state => state.user.mocids;
