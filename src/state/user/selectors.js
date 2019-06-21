3f1ac17942401dc682200d4cd3f310148af38518
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-21 10:19:54 -0700

    member link selector

--- src/state/user/selectors.js
+++ src/state/user/selectors.js
@@ -1,4 +1,23 @@
+import { createSelector } from 'reselect';
+import  {
+  getTownhallGovtrackId,
+} from '../townhall/selectors';
+
 export const getUid = state => state.user.uid;
 export const getUserEmail = state => state.user.email;
 export const getUserName = state => state.user.displayName;
-export const getUserMOCs = state => state.user.mocids;
+export const getUserMOCs = state => state.user.userMocs;
+
+export const getSelectedMemberLinks = state => createSelector([getUserMOCs] , (Members) => {
+  let curGovtrackcId = getTownhallGovtrackId(state);
+  console.log(curGovtrackcId)
+  let selectedMember = Members.filter((mem) => { return mem.govtrack_id === curGovtrackcId });
+  // TODO: Add selected moc to state!
+  selectedMember.moc_links ? Object.keys(selectedMember.moc_links).map(key => {
+    let link = selectedMember.moc_links[key];
+    return {
+      id: key,
+      ...link
+    }
+  }) : []
+});
