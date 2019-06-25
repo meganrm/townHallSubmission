10c84ddcdff759da6c9b7c1eacef4199b2719d11
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-25 11:38:06 -0700

    update user selector with selected mem. and link

--- src/state/user/selectors.js
+++ src/state/user/selectors.js
@@ -1,23 +1,17 @@
-import { createSelector } from 'reselect';
-import  {
-  getTownhallGovtrackId,
-} from '../townhall/selectors';
-
 export const getUid = state => state.user.uid;
 export const getUserEmail = state => state.user.email;
 export const getUserName = state => state.user.displayName;
 export const getUserMOCs = state => state.user.userMocs;
+export const getSelectedMember = state => state.user.selectedMember;
+export const getSelectedLink = state => state.user.selectedLink;
 
-export const getSelectedMemberLinks = state => createSelector([getUserMOCs] , (Members) => {
-  let curGovtrackcId = getTownhallGovtrackId(state);
-  console.log(curGovtrackcId)
-  let selectedMember = Members.filter((mem) => { return mem.govtrack_id === curGovtrackcId });
-  // TODO: Add selected moc to state!
-  selectedMember.moc_links ? Object.keys(selectedMember.moc_links).map(key => {
-    let link = selectedMember.moc_links[key];
-    return {
-      id: key,
-      ...link
-    }
-  }) : []
-});
+export const getSelectedMemberLinks = state => {
+  let selectedMember = getSelectedMember(state);
+    return selectedMember.moc_links ? Object.keys(selectedMember.moc_links).map(key => {
+      let link = selectedMember.moc_links[key];
+      return {
+        id: key,
+        ...link
+      }
+    }) : []
+};
