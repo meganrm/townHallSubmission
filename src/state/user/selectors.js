5050b1630f866c6136ce0f93fe54e18f5903c006
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-25 20:46:51 -0700

    remove MOC info from users selector

--- src/state/user/selectors.js
+++ src/state/user/selectors.js
@@ -2,16 +2,3 @@ export const getUid = state => state.user.uid;
 export const getUserEmail = state => state.user.email;
 export const getUserName = state => state.user.displayName;
 export const getUserMOCs = state => state.user.userMocs;
-export const getSelectedMember = state => state.user.selectedMember;
-export const getSelectedLink = state => state.user.selectedLink;
-
-export const getSelectedMemberLinks = state => {
-  let selectedMember = getSelectedMember(state);
-    return selectedMember.moc_links ? Object.keys(selectedMember.moc_links).map(key => {
-      let link = selectedMember.moc_links[key];
-      return {
-        id: key,
-        ...link
-      }
-    }) : []
-};
