f69f7bd26023dc3a7b141f197a190dbb8870ced5
Alan Magelssen <alanmagelssen@gmail.com>
2019-06-25 20:47:17 -0700

    clear link actions from user

--- src/state/user/actions.js
+++ src/state/user/actions.js
@@ -12,26 +12,6 @@ const setMOCs = payload => ({
   type: 'SET_MOCS',
 });
 
-const addLink = (payload) => ({
-  type: 'ADD_LINK',
-  payload,
-});
-
-const editLink = (payload) => ({
-  type: 'EDIT_LINK',
-  payload
-});
-
-const deleteLink = (payload) => ({
-  type: 'DELETE_LINK',
-  payload
-});
-
-export const setSelectedLink = (payload) => ({
-  type: 'SET_SELECTED_LINK',
-  payload
-})
-
 export const writeUserData = payload => (dispatch) => {
   firebasedb.ref(`users/${payload.uid}`).update({
     email: payload.email,
@@ -40,7 +20,7 @@ export const writeUserData = payload => (dispatch) => {
     .then(dispatch(setUser(payload)));
 };
 
-export const getUserMOCs = payload => (dispatch) => {
+export const startSetUserMocs = payload => (dispatch) => {
   firebasedb.ref(`users/${payload.uid}/mocs`).once('value').then((snapshot) => {
     const mocIds = Object.keys(snapshot.val());
     Promise.all(mocIds.map((id) => {
@@ -57,56 +37,5 @@ export const getUserMOCs = payload => (dispatch) => {
         dispatch(setMOCs(userMocData))
       })
       .catch((err) => console.log(err))
-  })
-}
-
-/******************* MOC LINK API******************************* */
-export const setSelectedMember = payload  => ({
-  payload,
-  type: 'SET_SELECTED_MEMBER',
-});
-
-export const getSelectedMemberInfo = payload => (dispatch) => {
-  firebasedb.ref(`mocData/${payload.govtrack_id}/helpful_links`).once('value').then((snapshot) => {
-    let links = snapshot.val();
-    for (let prop in links) {
-      links[prop].id = prop;
-    }
-    payload.moc_links = links;
-    dispatch(setSelectedMember(payload));
-  });
-}
-
-export const addMemberLink = payload => (dispatch) => {
-  if (payload.link_title === undefined || payload.link_url === undefined) {
-    return;
-  } else {
-    let link = {
-      link_title: payload.link_title,
-      url: payload.link_url
-    }
-    return firebasedb.ref(`mocData/${payload.member_id}/helpful_links`).push(link).then((ref) => {
-      let newLinkInfo = {
-        id: ref.key,
-        link
-      }
-      dispatch(addLink(newLinkInfo))
-    })
-  }
-}
-
-export const editMemberLink = payload => (dispatch) => {
-  let link = {
-    link_title: payload.linkInfo.link_title,
-    url: payload.linkInfo.url
-  }
-  return firebasedb.ref(`mocData/${payload.moc_id}/helpful_links/${payload.link_id}`).update(link).then(() => {
-    dispatch(editLink(payload));
-  })
-}
-
-export const deleteMemberLink = payload => (dispatch) => {
-  return firebasedb.ref(`mocData/${payload.moc_id}/helpful_links/${payload.link_id}`).remove().then(() => {
-    dispatch(deleteLink(payload));
   })
 }
