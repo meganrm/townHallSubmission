8caa28851a041ef6575756b1c83f1e1b808a83df
meganrm <meganrm@gmail.com>
2019-07-23 18:31:08 -0700

    moved selectors around

--- src/state/members-candidates/selectors.js
+++ src/state/members-candidates/selectors.js
@@ -3,5 +3,7 @@ import { map, uniq } from 'lodash';
 
 export const getAllPeople = state => state.people.allPeople;
 export const getPeopleRequestError = state => state.people.error;
+export const getSelectedMemberId = state => state.people.selectedMember;
+export const getSelectedLink = state => state.people.selectedLink;
 
 export const getAllNames = createSelector([getAllPeople], people => uniq(map(people, 'nameEntered')));
