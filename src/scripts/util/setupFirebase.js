79d87c8966706ff71efdc6a2a91ab19c410535a0
meganrm <meganrm@gmail.com>
2020-01-07 11:28:52 -0800

    production version of firebase

--- src/scripts/util/setupFirebase.js
+++ src/scripts/util/setupFirebase.js
@@ -1,7 +1,9 @@
 // FIREBASE METHODS
 // Initialize Firebase
 
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 import 'firebase/firestore';
 // Required for side-effects
 
